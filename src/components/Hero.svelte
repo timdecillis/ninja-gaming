5afe0f2d8647a68b2e4a4e852c7267c2a84668e2
timdecillis <timdecillis@gmail.com>
2024-09-07 19:11:12 -0700

    added landing copy

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,6 +3,15 @@
 	import SectionWrapper from './SectionWrapper.svelte';
 </script>
 
-<SectionWrapper>
+<SectionWrapper id="foo">
 	<Header />
+	<div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10">
+		<h2
+			class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold"
+		>
+			<span class="text-indigo-400"> Gym </span>Training <span class="text-slate-600 line-through"
+			>Is Hard</span><br/>
+      Just Got <span class="text-indigo-400" >Easier</span>
+		</h2>
+	</div>
 </SectionWrapper>
