5561a4dd3fbe56aaf1118e6c84c52fadcdcd7616
timdecillis <timdecillis@gmail.com>
2024-09-09 21:48:04 -0700

    styled friend cards

--- src/lib/TopFriends.svelte
+++ src/lib/TopFriends.svelte
@@ -1,10 +1,36 @@
-<div class="grid grid-cols-4 grid-rows-2 justify-center items-center border-2 border-black rounded-md col-start-2 col-span-2 text-center p-4 m-4">
-  <div>Friend 1</div>
-  <div>Friend 2</div>
-  <div>Friend 3</div>
-  <div>Friend 4</div>
-  <div>Friend 5</div>
-  <div>Friend 6</div>
-  <div>Friend 7</div>
-  <div>Friend 8</div>
+<div
+	class="grid grid-cols-4 grid-rows-2 justify-center items-center border-2 border-black rounded-md col-start-2 col-span-2 text-center p-4 m-4"
+>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 1</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 2</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 3</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 4</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 5</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 6</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 7</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
+		<div>Friend 8</div>
+	</div>
 </div>
