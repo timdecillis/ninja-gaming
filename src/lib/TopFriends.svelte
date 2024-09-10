44e85f561c8a779116f44e0caa45b8a0a53a006d
timdecillis <timdecillis@gmail.com>
2024-09-09 20:32:24 -0700

    created top friends component

---
+++ src/lib/TopFriends.svelte
@@ -0,0 +1,10 @@
+<div>
+  <div>Friend 1</div>
+  <div>Friend 2</div>
+  <div>Friend 3</div>
+  <div>Friend 4</div>
+  <div>Friend 5</div>
+  <div>Friend 6</div>
+  <div>Friend 7</div>
+  <div>Friend 8</div>
+</div>
