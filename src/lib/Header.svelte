214bb588afe153578cec56dad7de7f8962e356d6
timdecillis <timdecillis@gmail.com>
2024-09-10 14:22:14 -0700

    created anchor tags

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,1 +1,5 @@
-<h1 class="text-center text-5xl font-bold m-8" >Welcome to TimSpace</h1>
+<div class="flex">
+	<h1 class="text-center text-5xl font-bold m-8">Welcome to TimSpace</h1>
+  <a href="/">My Profile</a>
+  <a href="/settings">Settings</a>
+</div>
