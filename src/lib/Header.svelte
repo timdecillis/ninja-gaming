ffa97513167930db33ddb327f9625f87ff217068
timdecillis <timdecillis@gmail.com>
2024-09-10 15:02:39 -0700

    created settings page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,7 @@
-<div class="flex">
+<div class="flex justify-center items-center border-black border">
 	<h1 class="text-center text-5xl font-bold m-8">Welcome to TimSpace</h1>
-  <a href="/">My Profile</a>
-  <a href="/settings">Settings</a>
+	<div class="justify-self-end">
+		<a href="/">My Profile</a>
+		<a href="/settings">Settings</a>
+	</div>
 </div>
