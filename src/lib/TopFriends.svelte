a3159cceae5ac81537d3742c6fb013d8fe1f086f
timdecillis <timdecillis@gmail.com>
2024-09-10 13:10:40 -0700

    added FriendCard to TopFriends

--- src/lib/TopFriends.svelte
+++ src/lib/TopFriends.svelte
@@ -1,36 +1,10 @@
+<script>
+	import FriendCard from './FriendCard.svelte';
+</script>
+
 <div
 	class="grid grid-cols-4 grid-rows-2 justify-center items-center border-2 border-black rounded-md col-start-2 col-span-2 text-center p-4 m-4"
 >
-	<div class="friend-card">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 1</div>
-	</div>
-	<div class="friend-card">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 2</div>
-	</div>
-	<div class="flex flex-col items-center">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 3</div>
-	</div>
-	<div class="flex flex-col items-center">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 4</div>
-	</div>
-	<div class="flex flex-col items-center">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 5</div>
-	</div>
-	<div class="flex flex-col items-center">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 6</div>
-	</div>
-	<div class="flex flex-col items-center">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 7</div>
-	</div>
-	<div class="flex flex-col items-center">
-		<img class="h-12 w-10 rounded-2xl" src="src/lib/assets/Sibley(1).jpg" alt="me" />
-		<div>Friend 8</div>
-	</div>
+	<FriendCard />
+	<FriendCard />
 </div>
