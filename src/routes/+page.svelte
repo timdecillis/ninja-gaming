09c41df3442bb0457c1c462f844492ace6c951cd
timdecillis <timdecillis@gmail.com>
2024-09-09 20:33:36 -0700

    added profile and friends to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
+	import Profile from "$lib/Profile.svelte";
+	import TopFriends from "$lib/TopFriends.svelte";
+
 
 </script>
 
 <main class="flex flex-col">
-
+  <div>
+		<Profile/>
+		<TopFriends/>
+	</div>
 </main>
