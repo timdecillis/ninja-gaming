b1c67741bd2a3b80de2e3357b463efeec7f06566
timdecillis <timdecillis@gmail.com>
2024-09-11 11:39:09 -0700

    updated header position

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -2,6 +2,7 @@
 	import { count, increment, decrement, reset } from "./store";
 </script>
 
+
 <h3>The count is {$count}</h3>
 <button on:click={increment}>Add</button>
 <button on:click={decrement}>Subtract</button>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,6 @@
-<div class="flex justify-center items-center border-black border">
-	<h1 class="text-center text-5xl font-bold m-8">Welcome to TimSpace</h1>
-	<div class="justify-self-end">
+<div class="flex items-center border-black border">
+	<h1 class="text-center text-5xl font-bold m-8 relative left-[50%]">Welcome to TimSpace</h1>
+	<div>
 		<a href="/">My Profile</a>
 		<a href="/settings">Settings</a>
 	</div>
