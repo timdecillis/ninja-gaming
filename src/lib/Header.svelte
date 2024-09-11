dc1a1a19049ca43528cbc368ad75af0f85d36e19
timdecillis <timdecillis@gmail.com>
2024-09-11 11:51:15 -0700

    added buttons for anchors

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,7 @@
-<div class="flex items-center border-black border">
-	<h1 class="text-center text-5xl font-bold m-8 relative left-[50%]">Welcome to TimSpace</h1>
-	<div>
-		<a href="/">My Profile</a>
-		<a href="/settings">Settings</a>
+<div class="grid grid-cols-6 items-center border-black border">
+	<h1 class="text-center text-5xl font-bold m-8 col-start-3 col-span-2">Welcome to TimSpace</h1>
+	<div class="flex justify-around col-start-6 col-span-1 w-48">
+		<a href="/"><button>My Profile</button></a>
+		<a href="/settings"><button>Settings</button></a>
 	</div>
 </div>
