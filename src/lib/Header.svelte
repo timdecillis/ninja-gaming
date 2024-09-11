b7957e4683dfd043da084818eb640fe2188d7f67
timdecillis <timdecillis@gmail.com>
2024-09-11 11:56:08 -0700

    added button styles

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-6 items-center border-black border">
 	<h1 class="text-center text-5xl font-bold m-8 col-start-3 col-span-2">Welcome to TimSpace</h1>
-	<div class="flex justify-around col-start-6 col-span-1 w-48">
-		<a href="/"><button>My Profile</button></a>
-		<a href="/settings"><button>Settings</button></a>
+	<div class="flex justify-around col-start-6 col-span-1 w-52">
+		<a href="/"><button class="p-2 hover:border hover:bg-slate-200 rounded-lg transition">My Profile</button></a>
+		<a href="/settings"><button class="p-2 hover:border hover:bg-slate-200 rounded-lg transition">Settings</button></a>
 	</div>
 </div>
